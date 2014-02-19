97db844c2c1c316184125ed9270e9287c7074aae
Johannes Unterstein <j.unterstein@micromata.de>
2014-02-19 13:13:29 +0100

    introduced assert

--- src/test/java/info/unterstein/neo4j/quickstart/Neo4JTest.java
+++ src/test/java/info/unterstein/neo4j/quickstart/Neo4JTest.java
@@ -3,12 +3,15 @@ package info.unterstein.neo4j.quickstart;
 import info.unterstein.neo4j.quickstart.Neo4JServiceProvider;
 import info.unterstein.neo4j.quickstart.model.Organisation;
 import info.unterstein.neo4j.quickstart.model.User;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/application-context.xml"})
@@ -31,9 +34,11 @@ public class Neo4JTest {
     Organisation sub2sub1 = createOrganisation("Orga1 - Sub2 - Sub 1", sub2);
     User user1 = createUser("User1 -> Orga1 - Sub1", sub1);
     User user2 = createUser("User1 -> Orga1 - Sub2", sub2);
-    for (Organisation o : provider.organisationRepository.findOrganisationsForUser(user2)) {
-      System.out.println(o.name);
-    }
+    // expected:
+    // Orga1 - Sub2
+    // Orga1 - Sub2 - Sub 1
+    List<Organisation> organisationsForUser = provider.organisationRepository.findOrganisationsForUser(user2);
+    Assert.assertEquals(2, organisationsForUser.size());
   }
 
   private Organisation createOrganisation(String name, Organisation parent) {
