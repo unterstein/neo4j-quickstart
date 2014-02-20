6ffc6b670c1b94e7b3e7eca4f48edeedc2295754
Johannes Unterstein <j.unterstein@micromata.de>
2014-02-20 16:04:31 +0100

    works

--- src/main/java/info/unterstein/neo4j/quickstart/repository/OrganisationRepository.java
+++ src/main/java/info/unterstein/neo4j/quickstart/repository/OrganisationRepository.java
@@ -13,6 +13,6 @@ public interface OrganisationRepository extends GraphRepository<Organisation> {
   // first thought:
   // @Query("START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation RETURN organisation UNION MATCH organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*]-children RETURN children as organisation")
   // but was:
-  @Query("START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation RETURN organisation UNION MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*]-children RETURN children as organisation")
+  @Query("START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation RETURN organisation UNION START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*]-children RETURN children as organisation")
   public List<Organisation> findOrganisationsForUser(User user);
 }
