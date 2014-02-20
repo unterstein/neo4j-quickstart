acf9b36dd2131fea3996ce74bc51f36de7626423
Johannes Unterstein <j.unterstein@micromata.de>
2014-02-20 16:15:17 +0100

    blank line

--- src/main/java/info/unterstein/neo4j/quickstart/repository/OrganisationRepository.java
+++ src/main/java/info/unterstein/neo4j/quickstart/repository/OrganisationRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.neo4j.repository.GraphRepository;
 import java.util.List;
 
 public interface OrganisationRepository extends GraphRepository<Organisation> {
+
   @Query("START user=node({0})  MATCH user-[:" + Relations.ORGANISATION_USER + "]->organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*0..]-children RETURN children")
   public List<Organisation> findOrganisationsForUser(User user);
 }
