8a02b55b39dab7b69a3ad95d3dfb2966e506da53
Johannes Unterstein <j.unterstein@micromata.de>
2014-02-24 09:23:43 +0100

    added doc

--- src/main/java/info/unterstein/neo4j/quickstart/repository/OrganisationRepository.java
+++ src/main/java/info/unterstein/neo4j/quickstart/repository/OrganisationRepository.java
@@ -11,6 +11,11 @@ import java.util.List;
 
 public interface OrganisationRepository extends GraphRepository<Organisation> {
 
+  // first thought
+  // @Query("START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation RETURN organisation UNION MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*]-children RETURN children as organisation")
+  // but each UNION starts a new cypher, therefore this is correct:
+  // @Query("START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation RETURN organisation UNION START user=node({0}) MATCH user-[:" + Relations.ORGANISATION_USER + "]-organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*]-children RETURN children as organisation")
+  // but far to complicated, therefore use this fancy cypher: (thanks to michael hunger!)
   @Query("START user=node({0})  MATCH user-[:" + Relations.ORGANISATION_USER + "]->organisation<-[:" + Relations.ORGANISATION_ORGANISATION + "*0..]-children RETURN children")
   public List<Organisation> findOrganisationsForUser(User user);
 }
